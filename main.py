124acca1d69113ebbd2cc12c6c60fa684ccc55aa
keioanh <oanh_dpm245471@student.agu,edu,vn>
2025-11-28 19:31:42 +0700

    lần2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ root=Tk()
 root.title('Quản Lý Tuyến Du Lịch')
 root.minsize(height=500, width=600)
 Label(root, text='Ứng Dụng Quản Lý Tuyến Du Lịch', fg='red', font=('cambria', 14), width=30).grid(row=0)
-Listbox=Listbox(root, width=80, height=20),grid(row=1, columnspan=2)
+Listbox=Listbox(root, width=80, height=20).grid(row=1, columnspan=2)
 
 
 
