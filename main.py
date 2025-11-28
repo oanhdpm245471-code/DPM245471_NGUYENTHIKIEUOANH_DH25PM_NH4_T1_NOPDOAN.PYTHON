2ac00cc9ff01ebc98968ada0d529de53035b9a8b
keioanh <oanh_dpm245471@student.agu,edu,vn>
2025-11-28 19:16:36 +0700

    lần1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 root=Tk()
 root.title('Quản Lý Tuyến Du Lịch')
-root.minsize(heght=500, width=500)
-
+root.minsize(height=500, width=500)
+Label(root, text='Ứng Dụng Quản Lý Tuyến Du Lịch', fg='red', font=('Timmes', 18), width=25).grid(row=0)
 root.mainloop()
