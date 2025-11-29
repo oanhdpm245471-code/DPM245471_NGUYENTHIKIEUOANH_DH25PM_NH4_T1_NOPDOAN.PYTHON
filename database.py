6f7d41239aa803e943e511d02418d20adc9839d5
keioanh <oanh_dpm245471@student.agu,edu,vn>
2025-11-29 21:13:17 +0700

    lần3

---
+++ database.py
@@ -0,0 +1,15 @@
+path='D:\QLTDL.txt'
+def save(line):
+    f=open(path, 'a', encoding='utf8')
+    f.writelines('\n')
+    f.close()
+
+def read(): 
+    dl=[]
+    f=open(path, 'r', encoding='utf8')
+    for i in f:
+        data=i.strip()
+        arr=data.strip('-')
+        dl.append(dl)
+    f.close()
+    return dl
--- main.py
+++ main.py
@@ -1,24 +1,55 @@
 from tkinter import *
+from database import *
+def add():
+    line=id.get()+'-'+name.get()+'-'+day.get()+'-'+money.get()
+    save(line)
+    show()
+def show():
+    dl=read()
+    Listbox.delete(0, END)
+    for i in dl:
+        Listbox.insert(END, i)
 root=Tk()
+id=StringVar()
+name=StringVar()
+day=StringVar()
+money=StringVar()
 root.title('Quản Lý Tuyến Du Lịch')
 root.minsize(height=500, width=600)
 Label(root, text='Ứng Dụng Quản Lý Tuyến Du Lịch', fg='red', font=('cambria', 14), width=30).grid(row=0)
-Listbox=Listbox(root, width=95, height=20).grid(row=1, columnspan=2)
+Listbox=Listbox(root, width=95, height=20)
+Listbox.grid(row=1, columnspan=2)
+show()
 Label(root, text='Mã Tuyến: ').grid(row=2, column=0)
-Entry(root, width=40).grid(row=2, column=1)
+Entry(root, width=40, textvariable=id).grid(row=2, column=1)
 
 Label(root, text='Tên Tuyến: ').grid(row=3, column=0)
-Entry(root, width=40).grid(row=3, column=1)
+Entry(root, width=40, textvariable=name).grid(row=3, column=1)
 
 Label(root, text='Địa Điểm Xuất Phát: ').grid(row=4, column=0)
-Entry(root, width=40).grid(row=4, column=1)
+Entry(root, width=40, textvariable=name).grid(row=4, column=1)
 
+Label(root, text='Địa Điểm Đến: ').grid(row=5, column=0)
+Entry(root, width=40, textvariable=name).grid(row=5, column=1)
+
+Label(root, text='Số Ngày Tour: ').grid(row=6, column=0)
+Entry(root, width=40, textvariable=day).grid(row=6, column=1)
+
+Label(root, text='Giá Tiền: ').grid(row=7, column=0)
+Entry(root, width=40, textvariable=money).grid(row=7, column=1)
 button=Frame(root)
-Button(button, text='Thêm').pack(side=LEFT)
+Button(button, text='Thêm', command=add).pack(side=LEFT)
 Button(button, text='Xóa').pack(side=LEFT)
 Button(button, text='Sửa').pack(side=LEFT)
 Button(button, text='Hủy').pack(side=LEFT)
 Button(button, text='Lưu').pack(side=LEFT)
 Button(button, text='Thoát', command=root.quit).pack(side=LEFT)
-button.grid(row=5, column=1)
+button.grid(row=8, column=1)
+
 root.mainloop()
+
+
+
+
+
+
