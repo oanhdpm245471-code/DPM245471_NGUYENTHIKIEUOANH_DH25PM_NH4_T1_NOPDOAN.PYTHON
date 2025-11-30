1f324866743c05fbd34721ba3b67359e1f1332f5
keioanh <oanh_dpm245471@student.agu,edu,vn>
2025-11-30 17:41:20 +0700

    lần3

--- database.py
+++ database.py
@@ -1,15 +1,20 @@
 path='D:\QLTDL.txt'
 def save(line):
+
+   
     f=open(path, 'a', encoding='utf8')
     f.writelines('\n')
     f.close()
 
 def read(): 
     dl=[]
+
+    
     f=open(path, 'r', encoding='utf8')
     for i in f:
         data=i.strip()
         arr=data.strip('-')
-        dl.append(dl)
+        dl.append(arr)
     f.close()
+
     return dl
